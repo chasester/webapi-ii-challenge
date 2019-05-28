bf3285f63a7553a36c48ec52deb287ee994a870e
Chase Wex <clapingmanly@hotmail.com>
2019-05-28 19:01:35 -0400

    updated code with the rest of the functionality

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 // implement your API here
 const express = require('express'); // built in node.js module to handle http traffic
-const db = require('./data/db.js');
 const hostname = '127.0.0.1'; // the local computer where the server is running
 const port = 3000; // a port we'll use to watch for traffic
 const cors = require('cors');
 const server = express();
-
-server.listen(port, hostname, () => {
-  // start watching for connections on the port specified
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const postsRoutes = require('./routes/post');
 
 server.use(express.json());
 server.use(cors());
@@ -19,5 +14,10 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
+server.use('/api/posts', postsRoutes);
+server.use('/', (req, res) => { res.send('api is up and running')});
 
-server.get('/',  cors(corsOptions), (req, res) => { res.send('Hello from Express')});
+server.listen(port, hostname, () => {
+    // start watching for connections on the port specified
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
